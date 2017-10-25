90f616fa14292de6e6ed559570ce627632dd6968
thejefftrent <thejefftrent@gmail.com>
2017-10-25 10:51:21 -0600

    finished child checking functionality

--- gitgudscrub.py
+++ gitgudscrub.py
@@ -1,4 +1,6 @@
 import praw
+from time import sleep, time
+
 import config
 gitgud = praw.Reddit(client_id=config.client_id,
                      client_secret=config.client_secret,
@@ -15,14 +17,21 @@ print(gitgud.user.me())
 
 for comment in comments:
     text = comment.body
+    already_reply = False
     if 'git gud' in text.lower():
-        already_reply = False
-        for child in comment.replies:
+        comment.refresh()
+        replies = comment.replies
+        for child in replies:
+            print(child.author)
             if config.username == child.author:
                 already_reply = True
-        if not already_reply:
-            try:
-                comment.reply(message)
-                print("we did it!")
-            except:
-                print("ruh roh raggie")
+                break
+        if already_reply:
+            continue
+        try:
+            comment.reply(message)
+            print("we did it!")
+        except:
+            print("ruh roh raggie")
+        print("I'm tired")
+        sleep((10 * 60) + 10) #10 min and 10 seconds to make sure we can post again
