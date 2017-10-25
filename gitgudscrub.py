69b9842d20b8a8318891b3b4c8bd462e0539f464
thejefftrent <thejefftrent@gmail.com>
2017-10-25 11:26:53 -0600

    Restructured code so it is pretty

--- gitgudscrub.py
+++ gitgudscrub.py
@@ -1,37 +1,45 @@
-import praw
 from time import sleep, time
 
+import praw
 import config
-gitgud = praw.Reddit(client_id=config.CLIENT_ID,
-                     client_secret=config.CLIENT_SECRET,
-                     password=config.PASSWORD,
-                     user_agent='git gud by /u/bhjeff',
-                     username=config.USERNAME)
+
 MESSAGE = "    git: 'gud' is not a git command. See 'git --help'.\n\n    The most similar command is\n            gui\n*****\n^^I ^^am ^^a ^^bot. ^^Visit ^^/r/gitgudscrubbot ^^for ^^more ^^information."
 
-sub = gitgud.subreddit('gitgudscrubbot')
+def main():
+    gitgud = praw.Reddit(client_id=config.CLIENT_ID,
+                         client_secret=config.CLIENT_SECRET,
+                         password=config.PASSWORD,
+                         user_agent='git gud by /u/bhjeff',
+                         username=config.USERNAME)
+    print(gitgud.user.me())
+    sub = gitgud.subreddit('gitgudscrubbot')
+    comments = sub.stream.comments()
+    for comment in comments:
+         if process_comment(comment):
+            # New accounts can only post once every 10 minues, remove the following if you don't have the restriction
+            print("I'm tired")
+            sleep((10 * 60) + 10)
+            print("I'm awake!")
 
-comments = sub.stream.comments()
-
-print(gitgud.user.me())
-
-for comment in comments:
-    text = comment.body
-    already_reply = False
-    if 'git gud' in text.lower():
-        comment.refresh()
-        replies = comment.replies
-        for child in replies:
-            print(child.author)
-            if config.USERNAME == child.author:
-                already_reply = True
-                break
-        if already_reply:
-            continue
-        try:
-            comment.reply(MESSAGE)
-            print("we did it!")
-        except:
-            print("ruh roh raggie")
-        print("I'm tired")
-        sleep((10 * 60) + 10) #10 min and 10 seconds to make sure we can post again
+def process_comment(comment):
+        text = comment.body
+        already_reply = False
+        if 'git gud' in text.lower():
+            comment.refresh()
+            replies = comment.replies
+            for child in replies:
+                print(child.author)
+                if config.USERNAME == child.author:
+                    already_reply = True
+                    break
+            if already_reply:
+                return False
+            try:
+                comment.reply(MESSAGE)
+                print("we did it!")
+            except:
+                print("ruh roh raggie")
+            return True
+        
+if __name__ == '__main__':
+    main()
