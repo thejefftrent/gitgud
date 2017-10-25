597cf8f8c5607bbc71145ed36aad408ef3963eff
thejefftrent <thejefftrent@gmail.com>
2017-10-25 09:45:31 -0600

    Removing commit history

---
+++ gitgudscrub.py
@@ -0,0 +1,20 @@
+import praw
+import config
+gitgud = praw.Reddit(client_id=config.client_id,
+                     client_secret=config.client_secret,
+                     password=config.password,
+                     user_agent='git gub by /u/bhjeff',
+                     username=config.username)
+message = "    git: 'gud' is not a git command. See 'git --help'.\n\n    The most similar command is\n            gui\n*****\n^^I ^^am ^^a ^^bot. ^^Visit ^^/r/gitgudscrubbot ^^for ^^more ^^information."
+
+sub = gitgud.subreddit('gitgudscrubbot')
+
+comments = sub.stream.comments()
+
+print(gitgud.user.me())
+
+for comment in comments:
+    text = comment.body
+    if 'git gud' in text.lower():
+        comment.reply(message)
+        print("we did it!")
